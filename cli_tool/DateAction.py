31e3c934ae505d636aa07a55f03ce0bfa59e3f8f
Giwan Lim <gw.lim@ascentnet.co.jp>
2021-04-19 12:15:52 +0900

    DateAction 테스트 완료

--- cli_tool/DateAction.py
+++ cli_tool/DateAction.py
@@ -2,90 +2,100 @@ from argparse import ArgumentParser, Action
 from datetime import timezone, timedelta, datetime
 import re
 
-RE_DATE = re.compile(r"^\d{8}$")
-RE_DATE_RANGE = re.compile(r"^\d{8}~\d{8}$")
+
+def _date_strftime_functer(format):
+    def _(d):
+        return d.strftime(format)
+
+    return _
+
+
+def _date_strptime_functer(format):
+    def _(d):
+        return datetime.strptime(d, format).date()
+
+    return _
+
 
 date_format = "%Y%m%d"
+date_ftime = _date_strftime_functer(date_format)
+date_ptime = _date_strptime_functer(date_format)
+
+
+RE_DATE = re.compile(r"^\d{8}$")
+RE_RANGED_DATE = re.compile(r"^\d{8}~\d{8}$")
 KST = timezone(timedelta(hours=9))
-today = datetime.now(KST).date()
-yesterday = today - timedelta(days=1)
-yesterday_str = yesterday.strftime(date_format)
+KST_today = datetime.now(KST).date()
+KST_today_str = date_ftime(KST_today)
+KST_yesterday = KST_today - timedelta(days=1)
+KST_yesterday_str = date_ftime(KST_yesterday)
 
 
-class FooAction(Action):
-    def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        if nargs is not None:
-            raise ValueError("nargs not allowed")
-        super(FooAction, self).__init__(option_strings, dest, **kwargs)
+def _filter_iter_functer(RE):
+    def _(iter):
+        return tuple([i for i in iter if RE.match(i)])
 
-    def __call__(self, parser, namespace, values, option_string=None):
-        print("%r %r %r" % (namespace, values, option_string))
-        setattr(namespace, self.dest, values)
+    return _
+
+
+filter_date_list = _filter_iter_functer(RE_DATE)
+filter_ranged_date_list = _filter_iter_functer(RE_RANGED_DATE)
+
+
+def parse_ranged_date(ranged_date):
+    st_date_str, en_date_str = ranged_date.split("~")
+    st_date = datetime.strptime(st_date_str, date_format).date()
+    en_date = datetime.strptime(en_date_str, date_format).date()
+    range_dates = []
+    if st_date > en_date:
+        st_date, en_date = en_date, st_date
+    range_dates = [
+        st_date + timedelta(days=i) for i in range((en_date - st_date).days)
+    ]
+    return tuple(range_dates)
+
+
+def parse_ranged_date_str(ranged_date):
+    parsed = parse_ranged_date(ranged_date)
+    return tuple([date_ftime(d) for d in parsed])
+
+
+def parse_ranged_date_str_list(ranged_date_list):
+    result = []
+    for ranged_date in ranged_date_list:
+        parsed = parse_ranged_date_str(ranged_date)
+        result = [*result, *parsed]
+    return tuple(result)
+
+
+def filter_days_str_before_today(days_str_list):
+    KST = timezone(timedelta(hours=9))
+    KST_today = datetime.now(KST).date()
+    return tuple([d for d in days_str_list if date_ptime(d) < KST_today])
+
+
+def drop_duplicated_date(date_list):
+    droped = set(date_list)
+    return tuple(droped)
+
+
+def parse_date_args(date_list):
+    plain_list = filter_date_list(date_list)
+    ranged_list = filter_ranged_date_list(date_list)
+    parsed_ranged = parse_ranged_date_str_list(ranged_list)
+    droped = drop_duplicated_date([*plain_list, *parsed_ranged])
+    validated = filter_days_str_before_today(droped)
+    return validated
 
 
 class DateAction(Action):
     def __init__(self, option_strings, dest, nargs="*", **kwargs):
         if nargs != "*":
-            print(f"{option_strings}의 nargs는 *모드로 강제 됩니다.")
+            # nargs는 *모드로 강제 됩니다
             nargs = "*"
         super(DateAction, self).__init__(
             option_strings, dest, nargs=nargs, **kwargs
         )
 
-    def parse_date_range(date_range_str):
-        st_date_str, en_date_str = date_range_str.split("~")
-        st_date = datetime.strptime(st_date_str, date_format).date()
-        en_date = datetime.strptime(en_date_str, date_format).date()
-        range_dates = []
-        if en_date == st_date:
-            range_dates.append(st_date)
-        else:
-            if st_date > en_date:
-                st_date, en_date = en_date, st_date
-            range_dates = [
-                st_date + timedelta(days=i)
-                for i in range((en_date - st_date).days)
-            ]
-        return range_dates
-
-    def get_date_range_list(dates):
-        return [date for date in dates if RE_DATE_RANGE.match(date)]
-
-    def get_date_list(dates):
-        return [date for date in dates if RE_DATE.match(date)]
-
     def __call__(self, parser, namespace, values, option_string=None):
-        setattr(namespace, self.dest, values)
-
-
-parser = ArgumentParser()
-parser.add_argument(
-    "-d", "--date", action=DateAction, dest="date_list", nargs="?"
-)
-args = parser.parse_args()
-print(args)
-
-
-"""
-parser = ArgumentParser(description="ACDP Kakao Clix CLI tool")
-parser.add_argument(
-    "-a",
-    "--apikey",
-    action="store",
-    help="API 액세스 키.",
-)
-parser.add_argument("-c", "--company", action="store", help="고객사 이름.")
-parser.add_argument(
-    "-d",
-    "--dates",
-    action="append",
-    dest="date_list",
-    default=yesterday_str,
-    help="YYYYMMDD 형식의 단일 일자. 여러번 호출 시 리스트 형태로 누적 됩니다.",
-)
-
-args = parser.parse_args()
-print(args.accumulate(args.integers))
-
-# https://greeksharifa.github.io/references/2019/02/12/argparse-usage/#dest-%EC%A0%81%EC%9A%A9-%EC%9C%84%EC%B9%98-%EC%A7%80%EC%A0%95
-"""
+        setattr(namespace, self.dest, parse_date_args(values))
--- tests/test_cli_tool.py
+++ tests/test_cli_tool.py
@@ -1,5 +1,142 @@
 from cli_tool import __version__
+from cli_tool.DateAction import (
+    filter_ranged_date_list,
+    filter_date_list,
+    parse_ranged_date_str,
+    parse_ranged_date_str_list,
+    filter_days_str_before_today,
+    drop_duplicated_date,
+    parse_date_args,
+    DateAction,
+)
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_filter_date_list():
+    target = "20200301"
+    test_list = tuple([target, "2020-03-01", "asdf"])
+    result_list = filter_date_list(test_list)
+    assert len(result_list) == 1
+    assert result_list[0] == target
+
+
+def test_filter_date_list_empty():
+    test_list = tuple(["2020-03-01", "asdf"])
+    result_list = filter_date_list(test_list)
+    assert len(result_list) == 0
+
+
+def test_filter_ranged_date_list():
+    target = "20200301~20200401"
+    test_list = tuple([target, "2020-03-01~2020-04-01", "asdf"])
+    result_list = filter_ranged_date_list(test_list)
+    assert len(result_list) == 1
+    assert result_list[0] == target
+
+
+def test_filter_ranged_date_list_empty():
+    test_list = tuple(["2020-03-01~2020-04-01", "asdf"])
+    result_list = filter_ranged_date_list(test_list)
+    assert len(result_list) == 0
+
+
+def test_parse_ranged_date_str_reverse():
+    ranged_date = "20200404~20200401"
+    target = tuple(["20200401", "20200402", "20200403"])
+    result = parse_ranged_date_str(ranged_date)
+    for i, d in enumerate(target):
+        assert result[i] == d
+
+
+def test_parse_ranged_date_str():
+    ranged_date = "20200401~20200404"
+    target = tuple(["20200401", "20200402", "20200403"])
+    result = parse_ranged_date_str(ranged_date)
+    for i, d in enumerate(target):
+        assert result[i] == d
+
+
+def test_parse_ranged_date_str_empty():
+    ranged_date = "20200401~20200401"
+    result = parse_ranged_date_str(ranged_date)
+    assert len(result) == 0
+
+
+def test_parse_ranged_date_str_list():
+    ranged_date_list = ["20200401~20200404", "20200301~20200304"]
+    target = tuple(
+        [
+            "20200401",
+            "20200402",
+            "20200403",
+            "20200301",
+            "20200302",
+            "20200303",
+        ]
+    )
+    result = parse_ranged_date_str_list(ranged_date_list)
+    for i, d in enumerate(target):
+        assert result[i] == d
+
+
+def test_parse_ranged_date_str_list_empty():
+    ranged_date_list = []
+    result = parse_ranged_date_str_list(ranged_date_list)
+    assert len(result) == 0
+
+
+def test_filter_days_str_before_today():
+    date_list = tuple(["20210418", "20210419", "20210420"])
+    target = date_list[0:1]
+    result = filter_days_str_before_today(date_list)
+    for i, d in enumerate(target):
+        assert result[i] == d
+
+
+def test_drop_duplicated_date():
+    date_list = tuple(["20210418", "20210419"])
+    date_list_2 = tuple([*date_list, "20210420"])
+    target = tuple([*date_list_2])
+    result = drop_duplicated_date([*date_list, *date_list_2])
+    assert len(target) == len(result)
+    for d in target:
+        assert d in result
+
+
+def test_parse_date_args():
+    date_list = tuple(
+        [
+            "20210418",
+            "20210419",
+            "20210420",
+            "20210417~20210422",
+            "2021-03-01",
+            "asdf",
+            "20210303",
+            "20210322",
+        ]
+    )
+    target = ["20210417", "20210418", "20210303", "20210322"]
+    result = parse_date_args(date_list)
+    assert len(result) == len(target)
+    for d in target:
+        assert d in result
+
+
+def test_DateAction():
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument("-d", "--date", action=DateAction)
+    target = ["20210417", "20210418", "20210303", "20210322"]
+    args = parser.parse_args(
+        "-d 20210418 20210419 20210420 20210417~20210422 2021-03-01 asdf 20210303 20210322".split()
+    )
+    arg_dict = dict(args._get_kwargs())
+
+    assert len(arg_dict["date"]) == len(target)
+    for d in target:
+        assert d in arg_dict["date"]
